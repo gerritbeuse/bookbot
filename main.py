a55ce0d129f87f3ff06ba286ba5db24a2bc9993d
Gerrit Beuse <gerrit.beuse95@gmail.com>
2025-07-17 22:52:15 +0200

    added analysis and report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import count_words
+from stats import count_characters
+from stats import sort_char_dict
 
 
 def get_book_text(path):
@@ -7,11 +9,23 @@ def get_book_text(path):
 
 def main():
 
-    relative_path = "./books/frankenstein.txt"
+    relative_path = "books/frankenstein.txt"
 
     text = get_book_text(relative_path)
     num_words = count_words(text)
+    char_dict = count_characters(text)
+    char_list_decending = sort_char_dict(char_dict)
 
-    print(f"{num_words} words found in the document")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {relative_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    
+    for entry in char_list_decending:
+        if entry["char"].isalpha():
+            print(f"{entry["char"]}: {entry["num"]}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,29 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    lower_text = text.lower()
+
+    char_dict = {}
+    
+    for char in lower_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_char_dict(char_dict):
+    char_list = []
+
+    for key, value in char_dict.items():
+        char_dict_item = {"char":key, "num":value}
+        char_list.append(char_dict_item)
+
+    char_list.sort(reverse=True, key=sort_on)
+    
+    return char_list
