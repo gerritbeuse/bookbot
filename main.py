66b05c17ca33c3316d11ce7ecbc34afe65b0cd2d
Gerrit Beuse <gerrit.beuse95@gmail.com>
2025-07-18 16:37:35 +0200

    added report and dynamic book selection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import count_words
 from stats import count_characters
 from stats import sort_char_dict
+import sys
 
 
 def get_book_text(path):
@@ -9,23 +10,28 @@ def get_book_text(path):
 
 def main():
 
-    relative_path = "books/frankenstein.txt"
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
 
-    text = get_book_text(relative_path)
-    num_words = count_words(text)
-    char_dict = count_characters(text)
-    char_list_decending = sort_char_dict(char_dict)
+        relative_path = sys.argv[1]
 
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {relative_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    
-    for entry in char_list_decending:
-        if entry["char"].isalpha():
-            print(f"{entry["char"]}: {entry["num"]}")
+        text = get_book_text(relative_path)
+        num_words = count_words(text)
+        char_dict = count_characters(text)
+        char_list_decending = sort_char_dict(char_dict)
 
-    print("============= END ===============")
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {relative_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        
+        for entry in char_list_decending:
+            if entry["char"].isalpha():
+                print(f"{entry["char"]}: {entry["num"]}")
 
-main()
+        print("============= END ===============")
+
+main() 
